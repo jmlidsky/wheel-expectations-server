1e6c43971b0ab6a03c2f507dd0ed56e559a59318
Jill Lidsky <jilllidsky15@gmail.com>
2020-05-21 10:13:57 -0400

    removed client origin

--- src/app.js
+++ src/app.js
@@ -17,11 +17,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
-)
+app.use(cors())
 
 app.use('/api/bikes', bikesRouter)
 app.use('/api/parts', partsRouter)
