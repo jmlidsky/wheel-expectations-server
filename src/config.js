3b9bbff021b05ad4979a1c3127a44cc048bada19
Jill Lidsky <jilllidsky15@gmail.com>
2020-05-21 20:44:27 -0400

    edited config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || `postgresql://${process.env.DBUSER}:${process.env.DBPASSWORD}@localhost/wheel-expectations`,
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || `postgresql://${process.env.DBUSER}:${DBPASSWORD}@localhost/wheel-expectations-test`,
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://jill:password@localhost/wheel-expectations',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://jill:password@localhost/wheel-expectations-test',
 }
